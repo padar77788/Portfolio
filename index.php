9af2a18a172436cd5266295b90ad7fdaccec6279
padar chandro roy <46196178+padar77788@users.noreply.github.com>
2020-09-10 00:16:38 +0600

    Add files via upload
    new file added

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+         
+
+
+
+<body>
+<div name='form'>
+<h2>The XMLHttpRequest Object</h2>
+
+<form action="" method='post' > 
+  <select name="customers" onchange="showCustomer(this.value)">
+    <option value="">Select a customer:</option>
+    <option value="Alfreds">Alfreds Futterkiste</option>
+    <option value="mamun">mamun</option>
+  </select>
+</form>
+<br>
+<div id="txtHint"><h2>Customer info will be listed here...</h2></div>
+
+</div>
+
+
+<script src='test.js'>
+
+</script>
+   
+    </body>
+</html>
+ 
+
+ 
+
